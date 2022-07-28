e5477b7840a41dc57db0bed107d7eb9051c26167
Reda E <reda.elmasdouri@gmail.com>
2022-07-28 16:16:59 +0200

    Importation des fichiers json. Affichage des quizz fonctionnel. Numérotation des questions

--- questionnaire.py
+++ questionnaire.py
@@ -19,31 +19,53 @@
 #    - lancer()
 #
 
+import json
+from operator import le
+from turtle import st
+
+import unicodedata
+
+from numpy import diff
+
+X =0
 class Question:
-    def __init__(self, titre, choix, bonne_reponse):
+    def __init__(self, categorie, titre, difficulte):
         self.titre = titre
-        self.choix = choix
-        self.bonne_reponse = bonne_reponse
+        self.categorie = categorie
+        self.difficulte = difficulte
+        
 
     def FromData(data):
         # ....
         q = Question(data[2], data[0], data[1])
         return q
 
+    def strip_accents(self, s):
+        return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
+
+    def get_quizz_filename(self, categorie, titre, difficulte):
+        return self.strip_accents(categorie).lower().replace(" ", "") + "_" + self.strip_accents(titre).lower().replace(" ", "") + "_" + self.strip_accents(difficulte).lower().replace(" ", "") + ".json"
+    def afficher_choix(self, choix):
+        for i in range(0, len(choix)):
+            print(str(i+1), '-', choix[i][0])
     def poser(self):
+        f = open(self.get_quizz_filename(self.categorie, self.titre, self.difficulte), 'r')
+        self.file_des = json.load(f)
+        f.close
         print("QUESTION")
         print("  " + self.titre)
-        for i in range(len(self.choix)):
-            print("  ", i+1, "-", self.choix[i])
+        for i in range(len(self.file_des["questions"])):
+            print("  ", i+1, "-", self.file_des["questions"][i]["titre"])
 
-        print()
-        resultat_response_correcte = False
-        reponse_int = Question.demander_reponse_numerique_utlisateur(1, len(self.choix))
-        if self.choix[reponse_int-1].lower() == self.bonne_reponse.lower():
-            print("Bonne réponse")
-            resultat_response_correcte = True
-        else:
-            print("Mauvaise réponse")
+            print()
+            self.afficher_choix(self.file_des["questions"][i]["choix"])
+            resultat_response_correcte = False
+            reponse_int = Question.demander_reponse_numerique_utlisateur(1, len(self.file_des["questions"][i]["choix"]))
+            if self.file_des["questions"][i]["choix"][reponse_int-1][1] == True:
+                print("Bonne réponse")
+                resultat_response_correcte = True
+            else:
+                print("Mauvaise réponse")
             
         print()
         return resultat_response_correcte
@@ -66,10 +88,15 @@ class Questionnaire:
 
     def lancer(self):
         score = 0
+        
         for question in self.questions:
             if question.poser():
                 score += 1
-        print("Score final :", score, "sur", len(self.questions))
+        print("Score final :", score, "sur", len(self.file_des["questions"]))
+        #for question in file_des["questions"]:
+        #    if question.poser():
+        #        score += 1
+        #print("Score final :", score, "sur", len(self.questions))
         return score
 
 
@@ -90,10 +117,10 @@ lancer_questionnaire(questionnaire)"""
 
 Questionnaire(
     (
-    Question("Quelle est la capitale de la France ?", ("Marseille", "Nice", "Paris", "Nantes", "Lille"), "Paris"), 
-    Question("Quelle est la capitale de l'Italie ?", ("Rome", "Venise", "Pise", "Florence"), "Rome"),
-    Question("Quelle est la capitale de la Belgique ?", ("Anvers", "Bruxelles", "Bruges", "Liège"), "Bruxelles")
+    Question("animaux", "les chats", "confirme"),
+    Question("animaux", "les chats", "confirme")
     )
 ).lancer()
 
+# animaux_leschats_confirme
 
